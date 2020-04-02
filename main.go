b90aeb90eeb3443691faf646e5a6f95828942788
easychessanimations <chessbotroulette@gmail.com>
2020-04-02 11:41:35 +0200

    Move interpreter

--- main.go
+++ main.go
@@ -3,7 +3,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/easychessanimations/gochess/board"
 )
@@ -17,11 +21,48 @@ func main() {
 
 	b.Reset()
 
-	b.Print()
+	reader := bufio.NewReader(os.Stdin)
 
-	pslms := b.PslmsForAllPiecesOfColor(board.BLACK)
+	ok := true
 
-	for i, mbi := range b.MovesSortedBySan(pslms) {
-		fmt.Printf("%d. %s\n", i+1, mbi.Str)
+	for ok {
+		fmt.Println("")
+
+		b.Print()
+
+		pslms := b.PslmsForAllPiecesOfColor(b.Pos.Turn)
+
+		mb := b.MovesSortedBySan(pslms)
+
+		for i, mbi := range mb {
+			fmt.Printf("%d. %s ", i+1, mbi.Str)
+		}
+
+		fmt.Print("\n\n> ")
+		text, _ := reader.ReadString('\n')
+
+		text = strings.Trim(text, "\r\n")
+
+		i, err := strconv.ParseInt(text, 10, 32)
+
+		if err == nil {
+			move := mb[i-1].Move
+
+			b.Push(move)
+		} else {
+			if text == "d" {
+				b.Pop()
+			} else {
+				for _, mbi := range mb {
+					if mbi.Str == text {
+						move := mbi.Move
+
+						b.Push(move)
+					}
+				}
+			}
+		}
+
+		ok = text != "x"
 	}
 }
